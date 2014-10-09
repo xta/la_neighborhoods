a4724780babc6bdc2b0ffb4185542aa953bd82a5
Rex Feng <rexfeng@gmail.com>
2014-10-08 23:33:55 -0700

    replace index page with hover map to navigate to #show page

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,11 +1,22 @@
+require 'json'
+
 class NeighborhoodsController < ApplicationController
 
   def index
     @neighborhoods = Neighborhood.includes(:crime).includes(:profiles).all
+    @geojson = all_geojson
   end
 
   def show
-    @neighborhood = Neighborhood.find(params[:id])
+    @neighborhood = Neighborhood.find_by_name(params[:id])
+    @neighborhood = Neighborhood.find(params[:id]) if @neighborhood.nil?
+  end
+
+  private
+
+  def all_geojson
+    polygons_data_path = Rails.root.join('data', 'la_county_simplified.json')
+    File.read(polygons_data_path)
   end
 
 end
