f7a629af415dfdb9b7b5426693558f65664477a1
Rex Feng <rexfeng@gmail.com>
2014-10-09 17:35:24 -0700

    update frontend to show neighborhood info

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -3,4 +3,12 @@ class Neighborhood < ActiveRecord::Base
 
   has_many :profiles
   has_one :crime
+
+  PROFILE_REJECT_LIST = %w(military population housing age families)
+
+  def show_profiles
+    self.profiles.reject do |profile|
+      PROFILE_REJECT_LIST.include?(profile.heading.downcase)
+    end
+  end
 end
